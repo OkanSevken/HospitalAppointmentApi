2d254dcba39a3add40896bebb8283afe4374ab40
Okan <okansevken20@gmail.com>
2024-03-04 10:22:12 +0300

    Entityler oluşturuldu.

---
+++ Core/HospitalApi.Domain/Entities/Appointment.cs
@@ -0,0 +1,35 @@
+﻿using HospitalApi.Domain.Cammon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApi.Domain.Entities
+{
+    public class Appointment : EntityBase
+    {
+        public Appointment()
+        {
+            
+        }
+
+        public Appointment(int patientId, DateTime appointmentDate, TimeSpan appointmentTime, int doctorId , string description, bool isApproved)
+        {
+            PatientId = patientId;
+            AppointmentDate = appointmentDate;
+            AppointmentTime = appointmentTime;
+            DoctorId = doctorId;
+            Description = description;
+            IsApproved = isApproved;
+        }
+
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public string Description { get; set; }
+        public bool IsApproved { get; set; }
+        public ICollection<DoctorCheck> DoctorChecks { get; set; }
+    }
+}

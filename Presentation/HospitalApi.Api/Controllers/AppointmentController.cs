97ceb49b294fd31d9142baeb9351163bd45d2f94
Okan <okansevken20@gmail.com>
2024-03-05 17:03:27 +0300

    Unit Of Work deseni ve MediatR kütüphanesi eklendi.

---
+++ Presentation/HospitalApi.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using HospitalApi.Application.Features.Appointments.Queries.GetAllAppointments;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public AppointmentController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAppointments()
+        {
+            var response = await mediator.Send(new GetAllAppointmentsQueryRequest());
+            return Ok(response);
+        }
+    }
+}

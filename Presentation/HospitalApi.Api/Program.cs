c4e4895029713adf08a185ae673a229265a6fd47
Okan <okansevken20@gmail.com>
2024-03-08 16:52:54 +0300

    Identity kütüphanesi eklendi, JWT ve token servisler yazıldı.

--- Presentation/HospitalApi.Api/Program.cs
+++ Presentation/HospitalApi.Api/Program.cs
@@ -2,6 +2,8 @@ using HospitalApi.Persistence;
 using HospitalApi.Application;
 using HospitalApi.Mapper;
 using HospitalApi.Application.Exceptions;
+using HospitalApi.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,8 +22,38 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hospital API", Version = "v1", Description = "Hospital API swagger client." });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazip bosluk biraktiktan sonra Token'i Girebilirsiniz \r\n\r\n Örnegin: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+
+    });
+});
 
 
 var app = builder.Build();
